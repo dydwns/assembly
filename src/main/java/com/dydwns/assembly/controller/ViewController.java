01b080f213bffc6d60ccfa789573846664e248c1
dydwns <kaka2438@hanmail.net>
2024-07-14 22:37:54 +0900

    ✨ DB정보를 화면에 보여주기

--- src/main/java/com/dydwns/assembly/controller/ViewController.java
+++ src/main/java/com/dydwns/assembly/controller/ViewController.java
@@ -1,22 +1,29 @@
 package com.dydwns.assembly.controller;
 
+import com.dydwns.assembly.entity.Board;
+import com.dydwns.assembly.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Optional;
+
 @Controller
 public class ViewController {
     ApiController restController = new ApiController();
+
+    @Autowired
+    BoardRepository boardRepository;
 
     @RequestMapping("/money")
     public String money() {
         return "money";
     }
 
-
     @RequestMapping("/result")
-    public String result(Model model, @RequestParam(value="money") int money) {
+    public String result(Model model, @RequestParam(value = "money") int money) {
         model.addAttribute("money", money);
         model.addAttribute("cpu", restController.parts().cpu);
         model.addAttribute("ram", restController.parts().ram);
@@ -25,7 +32,12 @@ public class ViewController {
     }
 
     @RequestMapping("/board")
-    public String board(Model model, @RequestParam(value="id") int id) {
+    public String board(Model model, @RequestParam(value = "id") int id) {
+        Optional<Board> result = boardRepository.findById(Long.valueOf(id));
+
+        Board board = result.orElse(null);
+        model.addAttribute("board", board);
+
         return "board";
     }
 }
